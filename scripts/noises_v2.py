19530320cadeb12f3da9e0d9e21f334b1a20bdc7
mweuro <20mehow01@gmail.com>
2024-12-16 23:46:45 +0100

    update

---
+++ scripts/noises_v2.py
@@ -0,0 +1,225 @@
+import torch
+import numpy as np
+from typing import Union, Literal
+
+
+
+def _array_or_tensor(signal: np.array, 
+                     output_format: Literal['numpy', 'tensor'] = 'numpy') -> Union[np.ndarray, torch.Tensor]:
+    if output_format == 'numpy':
+        pass
+    elif output_format == 'tensor':
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        signal = torch.Tensor(signal).to(device)
+    else:
+        raise ValueError("Output format must be either 'numpy' or 'tensor'")
+
+    return signal
+        
+
+
+def _noise_psd(N, psd = lambda f: 1) -> np.ndarray:
+    X_white = np.fft.rfft(np.random.randn(N))
+    S = psd(np.fft.rfftfreq(N))
+    S = S / np.sqrt(np.mean(S**2))
+    X_shaped = X_white * S
+    return np.fft.irfft(X_shaped)
+
+
+
+
+
+
+class WhiteNoise:
+    def __init__(self, c: float) -> None:
+        self.c = c
+    
+    def __call__(self, 
+                 signal: Union[np.ndarray, torch.Tensor],
+                 *,
+                 output_format: Literal['numpy', 'tensor'] = 'numpy') -> Union[np.ndarray, torch.Tensor]:
+        
+        if type(signal) == torch.Tensor:
+            signal = signal.cpu().numpy()
+        
+        color_noise = _noise_psd(len(signal), lambda f: 1)
+        noised_signal = signal + self.c * color_noise
+        output_signal = _array_or_tensor(noised_signal, output_format = output_format)
+        
+        return output_signal
+
+
+
+class BlueNoise:
+    def __init__(self, c: float) -> None:
+        self.c = c
+    
+    def __call__(self, 
+                 signal: Union[np.ndarray, torch.Tensor],
+                 *,
+                 output_format: Literal['numpy', 'tensor'] = 'numpy') -> Union[np.ndarray, torch.Tensor]:
+        
+        if type(signal) == torch.Tensor:
+            signal = signal.cpu().numpy()
+        
+        color_noise = _noise_psd(len(signal), lambda f: np.sqrt(f))
+        noised_signal = signal + self.c * color_noise
+        output_signal = _array_or_tensor(noised_signal, output_format = output_format)
+        
+        return output_signal
+
+
+
+class VioletNoise:
+    def __init__(self, c: float) -> None:
+        self.c = c
+    
+    def __call__(self, 
+                 signal: Union[np.ndarray, torch.Tensor],
+                 *,
+                 output_format: Literal['numpy', 'tensor'] = 'numpy') -> Union[np.ndarray, torch.Tensor]:
+        
+        if type(signal) == torch.Tensor:
+            signal = signal.cpu().numpy()
+        
+        color_noise = _noise_psd(len(signal), lambda f: f)
+        noised_signal = signal + self.c * color_noise
+        output_signal = _array_or_tensor(noised_signal, output_format = output_format)
+        
+        return output_signal
+
+
+
+class BrownianNoise:
+    def __init__(self, c: float) -> None:
+        self.c = c
+    
+    def __call__(self, 
+                 signal: Union[np.ndarray, torch.Tensor],
+                 *,
+                 output_format: Literal['numpy', 'tensor'] = 'numpy') -> Union[np.ndarray, torch.Tensor]:
+        
+        if type(signal) == torch.Tensor:
+            signal = signal.cpu().numpy()
+        
+        color_noise = _noise_psd(len(signal), lambda f: 1 / np.where(f == 0, float('inf'), f))
+        noised_signal = signal + self.c * color_noise
+        output_signal = _array_or_tensor(noised_signal, output_format = output_format)
+        
+        return output_signal
+
+
+
+class PinkNoise:
+    def __init__(self, c: float) -> None:
+        self.c = c
+    
+    def __call__(self, 
+                 signal: Union[np.ndarray, torch.Tensor],
+                 *,
+                 output_format: Literal['numpy', 'tensor'] = 'numpy') -> Union[np.ndarray, torch.Tensor]:
+        
+        if type(signal) == torch.Tensor:
+            signal = signal.cpu().numpy()
+        
+        color_noise = _noise_psd(len(signal), lambda f: 1 / np.where(f == 0, float('inf'), np.sqrt(f)))
+        noised_signal = signal + self.c * color_noise
+        output_signal = _array_or_tensor(noised_signal, output_format = output_format)
+        
+        return output_signal
+    
+        
+
+class ImpulseNoise:
+    def __init__(self,
+                 *,
+                 low_threshold: float, 
+                 high_threshold: float, 
+                 size: float = 0.0005) -> None:
+        self.low_threshold = low_threshold
+        self.high_threshold = high_threshold
+        self.size = size
+
+    def __call__(self, 
+                 signal: Union[np.ndarray, torch.Tensor],
+                 *,
+                 output_format: Literal['numpy', 'tensor'] = 'numpy') -> Union[np.ndarray, torch.Tensor]:
+
+        if type(signal) == torch.Tensor:
+            signal = signal.cpu().numpy()
+            
+        noise_sample = np.random.default_rng().\
+            uniform(self.low_threshold * min(signal), 
+                    self.high_threshold * max(signal), 
+                    int(self.size * len(signal)))   
+        zeros = np.zeros(len(signal) - len(noise_sample))
+        noise = np.concatenate([noise_sample, zeros])
+        np.random.shuffle(noise)
+        noised_signal = signal + noise
+        output_signal = _array_or_tensor(noised_signal, output_format = output_format)
+        
+        return output_signal
+
+
+
+class FrequencyNoise:
+    def __init__(self,
+                 *,
+                 min_freq: int, 
+                 max_freq: int, 
+                 c: float = 0.03) -> None:
+        self.min_freq = min_freq
+        self.max_freq = max_freq
+        self.c = c
+
+    def __call__(self, 
+                 signal: Union[np.ndarray, torch.Tensor],
+                 samplerate: int = 22050,
+                 *,
+                 output_format: Literal['numpy', 'tensor'] = 'numpy') -> Union[np.ndarray, torch.Tensor]:
+        
+        if type(signal) == torch.Tensor:
+            signal = signal.cpu().numpy()
+            
+        def fftnoise(f: np.array) -> np.array:
+            f = np.array(f, dtype = 'complex')
+            Np = (len(f) - 1) // 2
+            phases = np.random.rand(Np) * 2 * np.pi
+            phases = np.cos(phases) + 1j * np.sin(phases)
+            f[1 : (Np + 1)] *= phases
+            f[-1 : (-1 - Np) : -1] = np.conj(f[1 : (Np + 1)])
+            return np.fft.ifft(f).real
+            
+        samples = len(signal)
+        freqs = np.abs(np.fft.fftfreq(samples, 1/samplerate))
+        f = np.zeros(samples)
+        idx = np.where(np.logical_and(freqs >= self.min_freq, freqs <= self.max_freq))[0]
+        f[idx] = 1
+        noised_signal = signal + self.c * fftnoise(f)
+        output_signal = _array_or_tensor(noised_signal, output_format = output_format)
+        
+        return output_signal
+
+
+
+class ClippingNoise:
+    def __init__(self,
+                 *,
+                 threshold: float) -> None:
+        self.threshold = threshold
+
+    def __call__(self, 
+                 signal: Union[np.ndarray, torch.Tensor],
+                 *,
+                 output_format: Literal['numpy', 'tensor'] = 'numpy') -> Union[np.ndarray, torch.Tensor]:
+        
+        if type(signal) == torch.Tensor:
+            signal = signal.cpu().numpy()
+        
+        boundary = self.threshold * max(signal)
+        noised_signal = signal.copy()
+        noised_signal[signal > boundary] = boundary
+        noised_signal[signal < -boundary] = -boundary
+        output_signal = _array_or_tensor(noised_signal, output_format = output_format)
+        
+        return output_signal
---
+++ scripts/visualizations.py
@@ -0,0 +1,161 @@
+import numpy as np
+import torch
+import librosa
+from .noises_v2 import *
+import matplotlib
+import matplotlib.pyplot as plt
+from typing import Union
+
+
+
+def _prepare_wave(signal: Union[np.ndarray, torch.Tensor], 
+                  sr: int = 22050, 
+                  title: str = 'Signal Wave', 
+                  ax: matplotlib.axes.Axes = None) -> None:
+    
+    if isinstance(signal, torch.Tensor):
+        signal = signal.cpu().numpy()
+    time_axis = (torch.arange(0, len(signal)) / sr)
+
+    if ax is None:
+        _, ax = plt.subplots(1, 1)
+    ax.plot(time_axis, signal, linewidth = 1)
+    ax.grid(True)
+    ax.set_xlim([0, time_axis[-1]])
+    ax.set_title(title)
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Energy')
+
+
+
+def _prepare_spectogram(signal: Union[np.ndarray, torch.Tensor], 
+                    sr: int = 22050, 
+                    n_fft: int = 2048,
+                    hop_length: int = 64,
+                    title: str = 'Signal Spectogram (dB)',
+                    ax: matplotlib.axes.Axes = None) -> None:
+    
+    if isinstance(signal, torch.Tensor):
+        signal = signal.cpu().numpy()
+        
+    S = librosa.stft(signal, n_fft = n_fft, hop_length = hop_length)
+    D = librosa.amplitude_to_db(abs(S), ref=np.max)
+    
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+    else:
+        fig = ax.get_figure()
+        
+    spec = librosa.display.specshow(D, sr = sr, hop_length = hop_length, 
+                             x_axis = 'time', 
+                             y_axis = 'log', 
+                             cmap = 'viridis',
+                             ax = ax)
+    ax.set_title(title)
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Frequency (Hz)')
+    
+    return spec
+
+
+
+def plot_wave(signal: Union[np.ndarray, torch.Tensor], 
+              *,
+              sr: int, 
+              title: str = 'Signal Wave',
+              figsize: tuple[int] = (15, 5)) -> None:
+    
+    _, ax = plt.subplots(1, 1, figsize = figsize)
+    _prepare_wave(signal, sr, title, ax = ax)
+    plt.show()
+
+
+
+def plot_spec(signal: Union[np.ndarray, torch.Tensor], 
+              *,
+              sr: int = 22050, 
+              n_fft: int = 2048,
+              hop_length: int = 64,
+              title: str = 'Signal Spectogram (dB)',
+              figsize: tuple[int] = (15, 5)) -> None:
+    
+    fig, ax = plt.subplots(1, 1, figsize = figsize)
+    spec = _prepare_spectogram(signal, sr, n_fft, hop_length, title, ax = ax)
+    
+    cbar_ax = fig.add_axes([0.2, -0.05, 0.6, 0.1])
+    cbar_ax.set_axis_off()
+    fig.colorbar(spec, ax = cbar_ax, format = '%+2.0f dB', orientation = "horizontal", fraction = 0.5)
+    plt.show()
+
+
+
+def plot_wave_and_spec(signal: Union[np.ndarray, torch.Tensor], 
+                       *,
+                       sr: int = 22050, 
+                       n_fft: int = 2048,
+                       hop_length: int = 64,
+                       title_wave: str = 'Signal Wave',
+                       title_spec: str = 'Signal Spectogram (dB)',
+                       figsize: tuple[int] = (15, 10)) -> None:
+    
+    fig, axs = plt.subplots(2, 1, figsize = figsize)
+    _prepare_wave(signal, sr, title_wave, ax = axs[0])
+    spec = _prepare_spectogram(signal, sr, n_fft, hop_length, title_spec, ax = axs[1])
+    cbar_ax = fig.add_axes([0.2, -0.05, 0.6, 0.1])
+    cbar_ax.set_axis_off()
+    fig.colorbar(spec, ax = cbar_ax, format = '%+2.0f dB', orientation = "horizontal", fraction = 0.5)
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+def plot_multiple_waves(signals: list[Union[np.ndarray, torch.Tensor]], 
+                        *,
+                        sr: int = 22050, 
+                        titles: list[str] = None,
+                        figsize: tuple[int] = None) -> None:
+    
+    if titles is None:
+        titles = len(signals) * [None]
+    if len(titles) != len(signals):
+        raise ValueError('Incorrect number of titles')
+    if figsize is None:
+        figsize = (15, 5*len(signals))
+    no_signals = len(signals)
+    
+    _, axs = plt.subplots(no_signals, 1, figsize = figsize)
+    for i, (signal, title) in enumerate(zip(signals, titles)):
+        _prepare_wave(signal, sr, title, ax = axs[i])
+        
+    plt.tight_layout()
+    plt.show()
+
+
+
+def plot_multiple_specs(signals: list[Union[np.ndarray, torch.Tensor]], 
+                        *,
+                        sr: int = 22050, 
+                        n_fft: int = 2048,
+                        hop_length: int = 64,
+                        titles: list[str] = None,
+                        figsize: tuple[int] = None) -> None:
+    
+    if titles is None:
+        titles = len(signals) * [None]
+    if len(titles) != len(signals):
+        raise ValueError('Incorrect number of titles')
+    if figsize is None:
+        figsize = (15, 5*len(signals))
+    no_signals = len(signals)
+    
+    fig, axs = plt.subplots(no_signals, 1, figsize = figsize)
+    for i, (signal, title) in enumerate(zip(signals, titles)):
+        spec = _prepare_spectogram(signal, sr, n_fft, hop_length, title, ax = axs[i])
+    cbar_ax = fig.add_axes([0.2, -0.05, 0.6, 0.1])
+    cbar_ax.set_axis_off()
+    fig.colorbar(spec, ax = cbar_ax, format = '%+2.0f dB', orientation = "horizontal", fraction = 0.5)
+     
+    
+    plt.tight_layout()
+    plt.show()
